39ebece8789cecbf24d13408eb21c90ca1e9a1a8
Thomas Casteleyn <thomas.casteleyn@super-visions.com>
2022-01-12 16:06:36 +0000

    Add helper script

---
+++ convert-all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# First argument is configuration location
+Dest=$(realpath $1)
+shift
+
+# Walk other arguments and check if the files contain traps
+Files=()
+for File
+do
+	if grep -q NOTIFICATION-TYPE "$File"; then
+		Files+=($(realpath $File))
+	fi
+done
+
+# Iterate the loop to read and print each array element
+cd $(dirname $0)
+for File in "${Files[@]}"
+do
+	./telegrafconvertmib -p $(dirname $File) -m $(basename $File) -d $Dest
+done
